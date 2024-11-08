46bc03418ef36d0a86a09072dfb5eaa6cc580681
AttilaTech01 <d.hains@attila-technologies.com>
2024-11-08 11:00:29 -0500

    Map feature with leaflet

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,7 @@
+import "leaflet/dist/leaflet.css";
+import type { Map as LeafletMap } from "leaflet";
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import type { Place } from "../api/Place";
 
 interface MapProps {
@@ -5,5 +9,24 @@ interface MapProps {
 }
 
 export default function Map({ place }: MapProps) {
-  return <div className="m-4">Map!</div>;
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[40.7, -74]}
+      zoom={12}
+      scrollWheelZoom
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && <Marker position={[place.latitude, place.longitude]} />}
+    </MapContainer>
+  );
 }
