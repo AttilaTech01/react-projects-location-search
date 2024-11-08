25389fa52a7040ecd098e9a576963e9ea3da81a2
AttilaTech01 <d.hains@attila-technologies.com>
2024-11-08 10:22:03 -0500

    Hooks and Interfaces ready for use

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,3 +1,33 @@
-export default function LocationSearch() {
-  return <div className="m-4">Search for a location !</div>;
+import { useState } from "react";
+import type { Place } from "../api/Place";
+
+interface LocationSearchProps {
+  onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [places, setPlaces] = useState<Place[]>([]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log("Need to search for ...", searchTerm);
+  };
+
+  return (
+    <div className="m-4">
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="searchTerm">
+          Search
+        </label>
+        <input
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="searchTerm"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
